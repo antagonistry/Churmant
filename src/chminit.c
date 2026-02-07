4dabb9d57caa007ac08971560f188dfb182c74bb
antagonistry <antagonistry@protonmail.com>
2026-02-08 01:13:03 +0700

    fix 'src/chminit.c' error

--- src/chminit.c
+++ src/chminit.c
@@ -7,10 +7,17 @@ end
 
 churmant_main
   if churmant_os == CHURMANT_WINDOWS then
-    system("wget https://github.com/antagonistry/Churmant/archive/master.zip -OutFile Churmant.zip");
-    system("unzip Churmant.zip -d ~");
-    system("powershell \"mv ~/Churmant-master ~/.Churmant\"");
-    system("start %USERPROFILE%\\.Churmant\\build.bat");
-    _putenv("%USERPROFILE%\\Churmant\\bin");
+    system("powershell \"wget https://github.com/antagonistry/Churmant/archive/master.zip -OutFile Churmant.zip\"");
+    system("powershell \"Expand-Archive Churmant.zip -DestinationPath Churmant -Force -ErrorAction SilentlyContinue\"");
+    system("powershell \"Move-Item Churmant\\Churmant-master %USERPROFILE%\\.Churmant\" -Force -ErrorAction SilentlyContinue\"");
+    system("powershell \"Remove-Item Churmant.zip -Force -ErrorAction SilentlyContinue\"");
+    system("powershell \"Remove-Item Churmant -Force -ErrorAction SilentlyContinue\"");
+  else
+    system("curl https://github.com/antagonistry/Churmant/archive/master.zip -L -o Churmant.zip");
+    system("unzip Churmant.zip -d Churmant");
+    system("mv Churmant/Churmant-master ~/.Churmant");
+    system("rm -rf Churmant.zip");
+    system("rm -rf Churmant");
   end
+  println("successfully cloned the repository to home folder");
 churmant_mend(recover)
